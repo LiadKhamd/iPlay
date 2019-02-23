14c7bf95a394c3ae050758eed6d3cb6665e30491
LiadKhamd <liadkh95@gmail.com>
2019-02-23 19:11:34 +0200

    Main logo

--- app/src/main/java/com/afeka/liadk/iplay/MainActivity.java
+++ app/src/main/java/com/afeka/liadk/iplay/MainActivity.java
@@ -4,10 +4,12 @@ package com.afeka.liadk.iplay;
   Created by liadk
  */
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
